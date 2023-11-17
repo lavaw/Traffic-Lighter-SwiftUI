e87255d8b1f26cf579c1a46b874a47f2a8392dc2
Rudolf Amirian <ronaldo679@gmail.com>
2023-11-17 16:32:52 +0300

    Added SWIFTUI Traffic Lighter

---
+++ swiftuiapp/ChangeButton.swift
@@ -0,0 +1,18 @@
+//
+//  ChangeButton.swift
+//  swiftuiapp
+//
+//  Created by Rudolf Amiryan on 17.11.23.
+//
+
+import SwiftUI
+
+struct ChangeButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChangeButton()
+}
---
+++ swiftuiapp/ColorLight.swift
@@ -0,0 +1,18 @@
+//
+//  ColorLight.swift
+//  swiftuiapp
+//
+//  Created by Rudolf Amiryan on 17.11.23.
+//
+
+import SwiftUI
+
+struct ColorLight: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ColorLight()
+}
