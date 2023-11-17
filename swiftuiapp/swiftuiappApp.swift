f3559e0f9cce7c3689f4ce301c9058c95959e8cd
Rudolf Amirian <ronaldo679@gmail.com>
2023-11-17 15:25:33 +0300

    Initial Commit

---
+++ swiftuiapp/swiftuiappApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftuiappApp.swift
+//  swiftuiapp
+//
+//  Created by Rudolf Amiryan on 17.11.23.
+//
+
+import SwiftUI
+
+@main
+struct swiftuiappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
